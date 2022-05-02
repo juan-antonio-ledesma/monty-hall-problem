76b1d55c330f185c6081e67a21daf8dec525f9f3
juanantonioledesma <juanantonioledesma@gmail.com>
2022-05-02 21:59:22 +0200

    Card advances and improvements

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,12 @@
 const Card = () => {
-  return <div className="card">Card</div>
+  return (
+    <div className="card">
+      <div className="card-inner">
+        <div className="card-front"></div>
+        <div className="card-back"></div>
+      </div>
+    </div>
+  )
 }
 
 export default Card
