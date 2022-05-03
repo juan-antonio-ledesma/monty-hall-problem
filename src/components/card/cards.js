da9cead6ec697daca22701e5a5cc32377db16fff
juanantonioledesma <juanantonioledesma@gmail.com>
2022-05-03 22:02:00 +0200

    Simplify randomIntFromInterval function

--- src/components/card/cards.js
+++ src/components/card/cards.js
@@ -1,5 +1,5 @@
 const randomIntFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * max) + min
 }
 
 export const setCards = () => {
