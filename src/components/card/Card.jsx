541ba8f44e6cac2cd31aadd8234a714048bb8e6a
juanantonioledesma <juanantonioledesma@gmail.com>
2022-05-04 23:10:23 +0200

    Adjustments and add interrogation badge

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,11 @@
-const Card = () => {
+const Card = ({ children }) => {
   return (
     <div className="card">
       <div className="card-inner">
-        <div className="card-front"></div>
-        <div className="card-back"></div>
+        <div className="card-front">
+          <div className="card-interrogationBadge">?</div>
+        </div>
+        <div className="card-back">{children}</div>
       </div>
     </div>
   )
