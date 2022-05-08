39c009dd7f0e21ff31c497fca0661f4bc4dee519
juanantonioledesma <juanantonioledesma@gmail.com>
2022-05-08 21:45:30 +0200

    re-style card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,42 +1,16 @@
 import Image from 'next/image'
-import car from '../../public/images/car.png'
+import carImage from '../../public/images/car.png'
+import goatImage from '../../public/images/goat.png'
 
 const Card = () => {
   return (
-    // <div className="card">
-    //   <div className="card-inner">
-    //     <div className="card-front">
-    //       <div className="card-interrogationBadge">?</div>
-    //     </div>
-    //     <div className="card-back">
-    //       <Image src={car} alt="car" width={180} height={180} />
-    //     </div>
-    //   </div>
-    // </div>
-
-    <div className="flipcards">
-      <div className="flipcard flipcard--red">
-        <div className="flipcard__inner">
-          <div className="__front"></div>
-          <div className="__back">
-            <p>⚔️</p>
-          </div>
+    <div className="card">
+      <div className="card-inner">
+        <div className="card-front">
+          <div className="card-interrogationBadge">?</div>
         </div>
-      </div>
-      <div className="flipcard flipcard--blue">
-        <div className="flipcard__inner">
-          <div className="__front"></div>
-          <div className="__back">
-            <p>👑</p>
-          </div>
-        </div>
-      </div>
-      <div className="flipcard flipcard--yellow">
-        <div className="flipcard__inner">
-          <div className="__front"></div>
-          <div className="__back">
-            <p>❤️</p>
-          </div>
+        <div className="card-back">
+          <Image src={goatImage} alt="car" width={100} height={100} />
         </div>
       </div>
     </div>
