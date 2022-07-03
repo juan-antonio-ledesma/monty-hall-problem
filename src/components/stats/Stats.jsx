2b086c899938d3054f69cf9101bb8ac0564a3057
juanantonioledesma <juanantonioledesma@gmail.com>
2022-07-03 19:23:16 +0200

    Stats advances

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -34,18 +34,48 @@ const Stats = ({ stats }) => {
   return (
     <div className="stats">
       <p>Games Played Counter: {gamesPlayedCounter}</p>
-      <p>Keep Card Car Counter: {keepCardCarCounter}</p>
+      {/* <p>Keep Card Car Counter: {keepCardCarCounter}</p>
       <p>Keep Card Goat Counter: {keepCardGoatCounter}</p>
       <p>Change Card Car Counter: {changeCardCarCounter}</p>
-      <p>Change Card Goat Counter: {changeCardGoatCounter}</p>
+      <p>Change Card Goat Counter: {changeCardGoatCounter}</p> */}
 
       <p>Keep card</p>
-      <div className="stats-bar">{percentageKeepCardCar}</div>
-      <div className="stats-bar">{percentageKeepCardGoat}</div>
+      <div className="stats-barContainer">
+        <div
+          className="stats-bar"
+          style={{ width: percentageKeepCardCar }}
+          aria-hidden="true"
+        />
+        {percentageKeepCardCar} | {keepCardCarCounter}
+      </div>
+
+      <div className="stats-barContainer">
+        <div
+          className="stats-bar"
+          style={{ width: percentageKeepCardGoat }}
+          aria-hidden="true"
+        />
+        {percentageKeepCardGoat} | {keepCardGoatCounter}
+      </div>
 
       <p>Change card</p>
-      <div className="stats-bar">{percentageChangeCardCar}</div>
-      <div className="stats-bar">{percentageChangeCardGoat}</div>
+      <div className="stats-barContainer">
+        <div
+          className="stats-bar"
+          style={{ width: percentageChangeCardCar }}
+          aria-hidden="true"
+        />
+        {percentageChangeCardCar} | {changeCardCarCounter}
+      </div>
+
+      <div className="stats-barContainer">
+        <div
+          className="stats-bar"
+          style={{ width: percentageChangeCardGoat }}
+          aria-hidden="true"
+        />
+        {percentageChangeCardGoat} | {changeCardGoatCounter}
+      </div>
     </div>
   )
 }
