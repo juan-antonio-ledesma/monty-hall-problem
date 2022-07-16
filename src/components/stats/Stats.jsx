d7ad9bb3dfae3a256e3c8d6236aa83c903b069bd
juanantonioledesma <juanantonioledesma@gmail.com>
2022-07-16 12:26:33 +0200

    Stats adjustments

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -38,38 +38,45 @@ const Stats = ({ stats }) => {
       <p className="stats-secondaryTitle">Games played: {gamesPlayedCounter}</p>
 
       <p className="stats-secondaryTitle">Keep card</p>
-
-      <div className="stats-barWrapper">
-        <div className="stats-bar" style={{ width: percentageKeepCardCar }}>
-          <div className="stats-counterWrapper">Cars: {keepCardCarCounter}</div>
-          <div className="stats-percentage">{percentageKeepCardCar}</div>
-        </div>
-      </div>
-
-      <div className="stats-barWrapper">
-        <div className="stats-bar" style={{ width: percentageKeepCardGoat }}>
-          <div className="stats-counterWrapper">
-            Goats: {keepCardGoatCounter}
+      <div className="stats-row">
+        <div className="stats-barWrapper">
+          <div className="stats-bar" style={{ width: percentageKeepCardCar }}>
+            <div className="stats-counterWrapper">
+              Cars: {keepCardCarCounter}
+            </div>
+            <div className="stats-percentage">{percentageKeepCardCar}</div>
           </div>
-          <div className="stats-percentage">{percentageKeepCardGoat}</div>
+        </div>
+
+        <div className="stats-barWrapper">
+          <div className="stats-bar" style={{ width: percentageKeepCardGoat }}>
+            <div className="stats-counterWrapper">
+              Goats: {keepCardGoatCounter}
+            </div>
+            <div className="stats-percentage">{percentageKeepCardGoat}</div>
+          </div>
         </div>
       </div>
 
       <p className="stats-secondaryTitle">Change card</p>
-
-      <div className="stats-barWrapper">
-        <div className="stats-bar" style={{ width: percentageChangeCardCar }}>
-          <div className="stats-counterWrapper">
-            Cars: {changeCardCarCounter}
+      <div className="stats-row">
+        <div className="stats-barWrapper">
+          <div className="stats-bar" style={{ width: percentageChangeCardCar }}>
+            <div className="stats-counterWrapper">
+              Cars: {changeCardCarCounter}
+            </div>
+            <div className="stats-percentage">{percentageChangeCardCar}</div>
           </div>
-          <div className="stats-percentage">{percentageChangeCardCar}</div>
         </div>
-      </div>
 
-      <div className="stats-barWrapper">
-        <div className="stats-bar" style={{ width: percentageChangeCardGoat }}>
-          <div>Goats: {changeCardGoatCounter}</div>
-          <div>{percentageChangeCardGoat}</div>
+        <div className="stats-barWrapper">
+          <div
+            className="stats-bar"
+            style={{ width: percentageChangeCardGoat }}
+          >
+            <div>Goats: {changeCardGoatCounter}</div>
+            <div>{percentageChangeCardGoat}</div>
+          </div>
         </div>
       </div>
     </div>
